6e46e9b8ebdcebca9e77664978201c2ac7c59a9a
kuldeepsaini02311@gmail.com <kuldeepsaini02311@gmail.com>
2024-05-16 12:36:15 +0530

    add: Settings page

---
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -0,0 +1,23 @@
+import ProfileForm from "@/components/forms/profile-form";
+import React from "react";
+
+type Props = {};
+
+const Settings = (props: Props) => {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-4xl sticky top-0 z-[10] p-6 bg-background/50 backdrop-blur-lg flex items-center border-b">
+        <span>Settings</span>
+      </h1>
+      <div className="flex flex-col gap-10 p-6">
+        <div>
+          <h2 className="text-2xl font-bold">User Profile</h2>
+          <p className="text-base text-white/50">Add or update information</p>
+        </div>
+        <ProfileForm/>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
